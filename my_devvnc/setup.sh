a63e63ddb78139de75b41f36545e9608e882f2e9
Odake-Atsushi <atsusatoakimeg04@gmail.com>
2024-05-10 16:08:26 +0000

    リモートデスクトップ環境設定用ファイル追加。（未確認）

---
+++ my_devvnc/setup.sh
@@ -0,0 +1,14 @@
+apt -y update
+apt -y upgrade
+apt -y autopurge
+
+apt -y install language-pack-ja-base language-pack-ja
+update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
+source /etc/default/locale
+
+useradd --password "pass" --create-home atsushi
+apt -y install sudo
+gpasswd -a atsushi sudo
+
+apt -y install xrdp ubuntu-desktop-minimal task-gnome-desktop task-japanese-gnome-desktop language-pack-gnome-ja-base language-pack-gnome-ja gnome-user-docs-ja fonts-noto-cjk-extra
+#apt -y install libreoffice-help-ja libreoffice-l10n-ja thunderbird-locale-ja
