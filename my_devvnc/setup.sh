ab5573206ffa34b7697fb089650fd209bf12e61c
Odake-Atsushi <atsusatoakimeg04@gmail.com>
2024-05-10 17:45:12 +0000

    変更

--- my_devvnc/setup.sh
+++ my_devvnc/setup.sh
@@ -7,8 +7,11 @@ update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 source /etc/default/locale
 
 useradd --password "pass" --create-home atsushi
-apt -y install sudo
+apt -y install sudo tzdata
 gpasswd -a atsushi sudo
 
-apt -y install xrdp ubuntu-desktop-minimal task-gnome-desktop task-japanese-gnome-desktop language-pack-gnome-ja-base language-pack-gnome-ja gnome-user-docs-ja fonts-noto-cjk-extra
+apt -y install ubuntu-desktop-minimal task-gnome-desktop task-japanese-gnome-desktop language-pack-gnome-ja-base language-pack-gnome-ja gnome-user-docs-ja fonts-noto-cjk-extra
 #apt -y install libreoffice-help-ja libreoffice-l10n-ja thunderbird-locale-ja
+
+apt -y install xrdp
+systemctl enable xrdp
