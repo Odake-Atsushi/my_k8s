73269091d61e5431f02c62f0ce2ba9d833cefe12
Odake-Atsushi <atsusatoakimeg04@gmail.com>
2024-05-11 06:12:58 +0000

    xfceに変更してconfigmapを使用するように変更した

--- my_devvnc/setup.sh
+++ my_devvnc/setup.sh
@@ -1,17 +1,24 @@
-apt -y update
-apt -y upgrade
-apt -y autopurge
+apt-get -y update
+apt-get -y upgrade
+apt-get -y autopurge
 
-apt -y install language-pack-ja-base language-pack-ja
+apt-get -y install language-pack-ja-base language-pack-ja
 update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 source /etc/default/locale
-
 useradd --password "pass" --create-home atsushi
-apt -y install sudo tzdata
+apt-get -y install sudo
 gpasswd -a atsushi sudo
+DEBIAN_FRONTEND=noninteractive apt-get -y install tzdata
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 
-apt -y install ubuntu-desktop-minimal task-gnome-desktop task-japanese-gnome-desktop language-pack-gnome-ja-base language-pack-gnome-ja gnome-user-docs-ja fonts-noto-cjk-extra
-#apt -y install libreoffice-help-ja libreoffice-l10n-ja thunderbird-locale-ja
+# apt-get -y install ubuntu-desktop-minimal task-gnome-desktop task-japanese-gnome-desktop language-pack-gnome-ja-base language-pack-gnome-ja gnome-user-docs-ja fonts-noto-cjk-extra
+apt-get -y install ubuntu-desktop-minimal task-xfce-desktop task-japanese-desktop fonts-noto-cjk-extra
 
-apt -y install xrdp
+# systemctl start gdm3
+systemctl start lightdm
+echo "Start Gnome!"
+
+apt-get -y install xrdp
 systemctl enable xrdp
+echo "Setup Finish!"
+xrdp
