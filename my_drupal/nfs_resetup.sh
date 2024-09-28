11222d9cf12da9463e6e0f25ff5e12a4ec60d99e
Odake-Atsushi <atsusatoakimeg04@gmail.com>
2024-09-29 01:08:21 +0900

    subPath追加

--- my_drupal/nfs_resetup.sh
+++ my_drupal/nfs_resetup.sh
@@ -3,15 +3,15 @@
 rm -rf /mnt/storage/drupal/data/*
 rm -rf /mnt/storage/drupal/db/*
 
-cd /mnt/storage/drupal/data
+# cd /mnt/storage/drupal/data
 
-mkdir modules profiles sites themes
-mkdir -p sites/default/files
+# mkdir modules profiles sites themes
+# mkdir -p sites/default/files
 
-chmod a+w modules/
-chmod a+w profiles/
-chmod a+w themes/
+# chmod a+w modules/
+# chmod a+w profiles/
+# chmod a+w themes/
 
-chmod a+w sites/
-chmod a+w sites/default/
-chmod a+w sites/default/files/
+# chmod a+w sites/
+# chmod a+w sites/default/
+# chmod a+w sites/default/files/
