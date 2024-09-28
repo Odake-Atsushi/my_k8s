b3e72ad4bde6757285f46b80a3f86c9c5d0d85b7
Odake-Atsushi <atsusatoakimeg04@gmail.com>
2024-09-29 00:02:59 +0900

    スクリプト変更

--- my_drupal/nfs_resetup.sh
+++ my_drupal/nfs_resetup.sh
@@ -8,6 +8,10 @@ cd /mnt/storage/drupal/data
 mkdir modules profiles sites themes
 mkdir -p sites/default/files
 
+chmod a+w modules/
+chmod a+w profiles/
+chmod a+w themes/
+
 chmod a+w sites/
 chmod a+w sites/default/
 chmod a+w sites/default/files/
