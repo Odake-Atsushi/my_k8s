86d4481206006a93e15c49447949eb8e737c2984
Odake-Atsushi <atsusatoakimeg04@gmail.com>
2025-02-05 15:35:37 +0000

    installスクリプト作成

---
+++ setup_install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+sudo snap remove --purge microk8s
+
+sudo apt -y autopurge snapd
+sudo apt update
+sudo apt -y install snapd
+
+rm -rf ~/.kube
+
+sudo snap install microk8s --classic --channel=1.31/stable
+
+sudo usermod -a -G microk8s $USER
+mkdir -p ~/.kube
+chmod 0700 ~/.kube
