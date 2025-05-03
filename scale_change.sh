7cdd539bdaf43c0a2d0dbde10adbeac1dc04cd93
Odake-Atsushi <atsusatoakimeg04@gmail.com>
2025-05-03 15:35:50 +0000

    設定変更スクリプト修正。動作未確認。

--- scale_change.sh
+++ scale_change.sh
@@ -5,7 +5,7 @@
 
 scale_replicas=0
 
-microk8s kubectl scale deployments test-nginx-deployment --replicas=$scale_replicas
+# microk8s kubectl scale deployments test-nginx-deployment --replicas=$scale_replicas
 # microk8s kubectl scale deployments drupal-deployment --replicas=$scale_replicas
 # microk8s kubectl scale deployments drupal-mariadb-deployment --replicas=$scale_replicas
 # microk8s kubectl scale deployments nextcloud-deployment --replicas=$scale_replicas
@@ -17,4 +17,4 @@ microk8s kubectl scale deployments test-nginx-deployment --replicas=$scale_repli
 # microk8s kubectl scale deployments nextcloud-redis-deployment --replicas=$scale_replicas
 microk8s kubectl scale deployments ollama-deployment --replicas=$scale_replicas
 microk8s kubectl scale deployments open-webui-deployment --replicas=$scale_replicas
-microk8s kubectl scale deployments searxng-deployment --replicas=$scale_replicas
+# microk8s kubectl scale deployments searxng-deployment --replicas=$scale_replicas
